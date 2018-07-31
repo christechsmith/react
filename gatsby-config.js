0842228314a7fd7f39317a848ec04e43a949144a
Chris Smith <chris@quasar.lan>
2018-07-30 17:35:42 -0700

    Initial commit.

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Design + Code 3 Test Site',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to < br/> design and code React apps</h1>
+    <p>Complete course about the best tools and design system. Prototype and build apps with React and Swift.</p>
+    <p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
